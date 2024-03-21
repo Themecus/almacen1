ff7a28e809e103d8deaa9f8c2c364df60198200d
Themecus <miguelangelbros2015@gmail.com>
2024-03-21 19:50:36 -0400

    referencia y inicio puto

---
+++ src/swexo/Main.java
@@ -0,0 +1,11 @@
+package swexo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("te oido con mi vida ucab");
+    }
+}
+
+
+
---
+++ src/swexo/jojo.java
@@ -0,0 +1,10 @@
+package swexo;
+
+public class jojo {
+    public static void main(String[] args) {
+
+        System.out.println("te oido con mi vida ucab");
+        int n=5,m=5;
+        System.out.println(n+m+"dsfff");
+    }
+}
